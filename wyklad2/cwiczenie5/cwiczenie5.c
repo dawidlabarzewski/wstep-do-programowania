1ff00e7dd5e8ce86729eee6e91bfff38c97aa6d9
dawidlabarzewski <154749@student.uwm.edu.pl>
2021-11-03 18:06:46 +0100

    [wyklad2] cwiczenie5

---
+++ wyklad2/cwiczenie5/cwiczenie5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int liczba = 10;
+
+    int dwukrotnosc = liczba * 2;
+
+    int kwadrat = liczba * liczba;
+
+    printf("liczba: %d\n", liczba);
+    printf("dwukrotnosc: %d\n", dwukrotnosc);
+    printf("liczba do kwadratu: %d\n", kwadrat);
+
+    return 0;
+}
