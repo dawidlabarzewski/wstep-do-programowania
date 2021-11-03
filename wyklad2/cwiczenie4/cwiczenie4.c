4e328312d7a7aa95513d46ca5410544dc403b817
dawidlabarzewski <154749@student.uwm.edu.pl>
2021-11-03 17:51:57 +0100

    [wyklad2] cwiczenie4

---
+++ wyklad2/cwiczenie4/cwiczenie4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void first() {
+    printf("Panie Janie!\n");
+}
+
+void second() {
+    printf("Rano wstan!\n");
+}
+
+int main() {
+    first();
+    first();
+    second();
+
+    return 0;
+}
